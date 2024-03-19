94cbec37b1db083f489b6edc248ede888e7f17b4
KeaCluster <zzabdiel29@gmail.com>
2024-03-19 16:54:52 -0600

    Implemented cobra

--- cli-todo/cmd/main.go
+++ cli-todo/cmd/main.go
@@ -1,31 +1,40 @@
 package cmd
 
 import (
-	"flag"
+	"cli-todo/internal/handler"
 	"fmt"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "cli-todo",
+	Short: "CLI Todo is a simple task manager (test)",
+}
+
 func main() {
-	addTask := flag.String("add", "", "Add a new task")
-	listTask := flag.Bool("list", false, "List all tasks")
-	completeTask := flag.Int("complete", 0, "Mark task as complete")
-	deleteTask := flag.Int("delete", 0, "Delete a task")
+	taskList := &handler.TaskList{}
 
-	flag.Parse()
+	var addCmd = &cobra.Command{
+		Use:   "add [description]",
+		Short: "Add a new task",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			description := args[0]
+			err := taskList.AddTask(description)
+			if err != nil {
+				fmt.Printf("Error adding task %s", err)
+				os.Exit(1)
+			}
+			fmt.Println("Task added successfully")
+		},
+	}
 
-	switch {
-	case *addTask != "":
-		fmt.Println("Adding task:", *addTask)
-	case *listTask:
-		fmt.Println("Listing all tasks", *listTask)
-	case *completeTask > 0:
-		fmt.Println("Task marked as completed", *completeTask)
-	case *deleteTask > 0:
-		fmt.Println("Task deleted", *deleteTask)
-	default:
-		fmt.Println("No valid Task specified")
-		flag.PrintDefaults()
+	rootCmd.AddCommand(addCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
