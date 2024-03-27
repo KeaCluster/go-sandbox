08fd494e5d891cbb393535ea173e866d0fdf8cc6
KeaCluster <zzabdiel29@gmail.com>
2024-03-26 22:34:12 -0600

    Basic logic

--- go-scraper/main.go
+++ go-scraper/main.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+func main() {
+	urlList := []string{
+		"www.wikileaks.org",
+		"www.wikipedia.org",
+	}
+
+	workers := 5
+	urls := make(chan string, len(urlList))
+	results := make(chan ScrapingRes, len(urlList))
+
+	for i := 0; i < workers; i++ {
+		go worker(urls, results)
+	}
+
+	for _, url := range urlList {
+		urls <- url
+	}
+	close(urls)
+
+	for i := 0; i < len(urlList); i++ {
+		result := <-results
+		if result.Err != nil {
+			fmt.Printf("Error scraping %s: %v\n", result.Url, result.Err)
+		} else {
+			fmt.Printf("Scraped %s: %s\n", result.Url, result.Title)
+		}
+	}
+}
--- go-scraper/scraper.go
+++ go-scraper/scraper.go
@@ -1,1 +1,12 @@
 package main
+
+func worker(urls <-chan string, results chan<- ScrapingRes) {
+	for url := range urls {
+		title, err := scrape(url)
+		results <- ScrapingRes{Url: url, Title: title, Err: err}
+	}
+}
+
+func scrape(url string) (string, error) {
+
+}
